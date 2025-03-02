b20ed4c953aac7d05a53448d784554610d356cd4
Siddhesh Lakhani <lakhani.siddhesh@gmail.com>
2025-03-02 22:30:29 +0530

    fix

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Page() {
   return (
     <div className="h-screen w-screen flex items-center justify-center">
-      skills
+      about
     </div>
   );
 }
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">
+      contact
+    </div>
+  );
+}
