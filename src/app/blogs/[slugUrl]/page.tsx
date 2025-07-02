43bf923c4d09a78433f91ab67f3edfa2104b0871
Siddhesh Lakhani <lakhani.siddhesh@gmail.com>
2025-07-02 09:43:42 +0530

    meta image for blog

--- src/app/blogs/[slugUrl]/page.tsx
+++ src/app/blogs/[slugUrl]/page.tsx
@@ -14,6 +14,7 @@ export async function generateMetadata(props: { params: Promise<{ slugUrl: strin
       blog?.description ??
       "Explore insightful blogs by Siddhesh Lakhani, where technology, coding, and design come together. Dive deep into innovative concepts and trends shaping the future of tech.",
     url: `https://sidlakhani.in/blogs/${slugUrl}`,
+    image: blog?.image ?? "./open-graph/poster-1.png",
   });
 }
 
